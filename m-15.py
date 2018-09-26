304c7a918a96b1d8453216a20da3bcd089c114b7
MuthumariP <43034703+MuthumariP@users.noreply.github.com>
2018-09-26 16:38:08 +0530

    Delete m-15.py

--- m-15.py
+++
@@ -1,8 +0,0 @@
-
-#15-DISPLAY EVEN NUMBERS BETWEEN 2 INTERVALS
-print("DISPLAY EVEN NUMBERS BETWEEN 2 INTERVALS")
-s=int(input("s="))
-e=int(input("e="))
-for x in range(s+1,e):
-    if(x%2==0):
-        print(x)
